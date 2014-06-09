9a72a4ca40ef85da174125820eacb83f05124680
PinguinitoRAwr <pinguinitorawr@gmail.com>
2014-06-09 15:08:35 -0500

    Auto Player v1

--- CookiePenguinPlayer.js
+++ CookiePenguinPlayer.js
@@ -1,11 +1,4 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
+
 var interval = 900;
 var autoBuy = true;
 var autoBuyTxt = 'on';
